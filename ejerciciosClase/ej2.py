ee54322f037ee5183b686dfe42b742d65b900636
JaimeCastanedo <mordis0812@gmail.com>
2025-12-03 11:21:43 +0100

    ej2Figuras

--- ejerciciosClase/ej2.py
+++ ejerciciosClase/ej2.py
@@ -8,3 +8,18 @@ Estrella 8 puntas
  *  *  *
 *   *   *
 """
+
+a = int(input("Dime la altura de la figura, debe ser un numero impar: "))
+
+
+for i in range(a):
+    for j in range(a):
+        if i == a // 2:
+            print("*", end="")
+        elif j == i or j == a // 2:
+            print("*", end="")
+        elif j == a - 1 - i:
+            print("*", end="")
+        else:
+            print(" ", end="")
+    print() 
